5cf6966ed572e97d679e113645c820252fd80642
meagwjy <me.agwjy@gmail.com>
2016-10-14 00:25:01 +0700

    register

---
+++ register.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register - Eskul</title>
+</head>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<body>
+<ul class="nav nav-pills">
+  <li role="presentation" class="active"><a href="#">Home</a></li>
+  <li role="presentation"><a href="#">Profile</a></li>
+  <li role="presentation"><a href="#">Messages</a></li>
+</ul>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
